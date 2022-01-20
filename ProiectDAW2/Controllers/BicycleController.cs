5558b0e693c0db99eaa214365fc745e14be9eb5e
raresluke98 <rares.damian-luca@numeract.com>
2022-01-20 08:43:20 +0200

    added controller

---
+++ ProiectDAW2/Controllers/BicycleController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProiectDAW2.Models;
+
+namespace ProiectDAW2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BicycleController : ControllerBase
+    {
+        private readonly BicycleDbContext _context;
+
+        public BicycleController(BicycleDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bicycle
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bicycle>>> GetBicycles()
+        {
+            return await _context.Bicycles.ToListAsync();
+        }
+
+        // GET: api/Bicycle/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bicycle>> GetBicycle(int id)
+        {
+            var bicycle = await _context.Bicycles.FindAsync(id);
+
+            if (bicycle == null)
+            {
+                return NotFound();
+            }
+
+            return bicycle;
+        }
+
+        // PUT: api/Bicycle/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBicycle(int id, Bicycle bicycle)
+        {
+            if (id != bicycle.BicycleId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bicycle).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BicycleExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bicycle
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Bicycle>> PostBicycle(Bicycle bicycle)
+        {
+            _context.Bicycles.Add(bicycle);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBicycle", new { id = bicycle.BicycleId }, bicycle);
+        }
+
+        // DELETE: api/Bicycle/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBicycle(int id)
+        {
+            var bicycle = await _context.Bicycles.FindAsync(id);
+            if (bicycle == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bicycles.Remove(bicycle);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BicycleExists(int id)
+        {
+            return _context.Bicycles.Any(e => e.BicycleId == id);
+        }
+    }
+}
