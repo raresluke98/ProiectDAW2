83d726bdaba6c0d39332e96d67211a77455c35c7
raresluke98 <rares.damian-luca@numeract.com>
2022-02-01 03:03:36 +0200

    added JwtMiddleware class

---
+++ ProiectDAW2/Authorization/JwtMiddleware.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectDAW2.Authorization
+{
+    public class JwtMiddleware
+    {
+        private readonly ReqeustDelegate _next;
+        private readonly AppSettings _appSettings;
+        
+        public JwtMiddleware(RequestDelegate next, IOptions<AppSettings> appSettings)
+        {
+            _next = next;
+            _appSettings = appSettings.Value;
+        }
+
+        public async Task Invoke(HttpContext context, IUserService userService, IJwtUtils jwtUtils)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var userId = jwtUtils.ValidateJwtToken(token);
+            if (userId != null)
+            {
+                // attach user to context on successful jwt validation
+                context.Items["User"] = userService.GetById(userId.Value);
+            }
+
+            await _next(context);
+        }
+    }
+}
