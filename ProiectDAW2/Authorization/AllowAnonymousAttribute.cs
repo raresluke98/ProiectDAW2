4b3cd3020c6338ba96ac11eb0312a2ae4ba3518e
raresluke98 <rares.damian-luca@numeract.com>
2022-02-01 02:37:27 +0200

    added AllowAnonymousAttribute class

---
+++ ProiectDAW2/Authorization/AllowAnonymousAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectDAW2.Authorization
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AllowAnonymousAttribute : Attribute
+    {
+
+    }
+}
