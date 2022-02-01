2c7a3fedf11bf5e95b585089b5ba1d13695d7f73
raresluke98 <rares.damian-luca@numeract.com>
2022-02-01 03:37:29 +0200

    added UserController class

---
+++ ProiectDAW2/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectDAW2.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("[action]")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+            return Ok(response);
+        }
+
+        [Authorize(Role.Admin)]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
+
+        [HttpGet("id:int")]
+        public IActionResult GetById(int id)
+        {
+            // only admins can access other user records
+            var currentUser = (user)HttpContext.Items["User"];
+            if (id != currentUser.Id && currentUser.Role != Role.Admin)
+                return Unauthorized(new { message = "Unauthorized" });
+
+            var user = _userService.GetById(id);
+            return Ok(user);
+        }
+    }
+}
