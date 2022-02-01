359b0846de6545e63442f4f2fbff6984e35cc58f
raresluke98 <rares.damian-luca@numeract.com>
2022-02-01 08:41:24 +0200

    added DataContext, ErrorHandlerMiddleware classes

---
+++ ProiectDAW2/Helpers/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ProiectDAW2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProiectDAW2.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        private readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // TODO: change to MSSQL DB
+            options.UseInMemoryDatabase("TestDb");
+        }
+    }
+}
